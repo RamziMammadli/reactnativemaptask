6f077ca9e211a97ffee29e608559f421f456c75d
RamziMammadli <remzimemmedli95@gmail.com>
2022-12-18 11:09:47 +0400

    Viewlarda adlarin paddini duzeldi

--- components/Box.js
+++ components/Box.js
@@ -1,22 +1,22 @@
 import { Text, StyleSheet, View } from "react-native";
 import React, { Component } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
-import {
-  faPerson,
-} from "@fortawesome/free-solid-svg-icons";
+import { faPerson } from "@fortawesome/free-solid-svg-icons";
 
-export default Box = ({name, description, rank, color}) => {
+export default Box = ({ name, description, rank, color }) => {
   return (
-    <View style={{...styles.card, backgroundColor: color}}>
-            <View style={styles.imgbox}>
-              <FontAwesomeIcon icon={faPerson} size={50}/>
-            </View>
-            <View>
-              <Text style={{fontSize:22}}>{name}</Text>
-              <Text>{description}</Text>
-            </View>
-            <Text style={{fontSize:22}}>{rank}</Text>
-          </View>
+    <View style={{ ...styles.card, backgroundColor: color }}>
+      <View style={{flexDirection:'row', alignItems:'center'}}>
+        <View style={styles.imgbox}>
+          <FontAwesomeIcon icon={faPerson} size={50} />
+        </View>
+        <View style={{paddingLeft:10}}>
+          <Text style={{ fontSize: 20 }}>{name}</Text>
+          <Text>{description}</Text>
+        </View>
+      </View>
+      <Text style={{ fontSize: 22 }}>{rank}</Text>
+    </View>
   );
 };
 
@@ -28,9 +28,9 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-between",
-    marginVertical:15,
-    borderRadius:20  
-},
+    marginVertical: 15,
+    borderRadius: 20,
+  },
   imgbox: {
     borderRadius: 100,
     backgroundColor: "white",
